f9bb2eb64609701d5244ccd2700b90b7f4330ec2
Bullets-jyr <jyr@bullets.co.kr>
2024-01-08 21:33:32 +0900

    25. StateProvider - family, autoDispose.family

---
+++ lib/pages/family/family_provider.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final familyCounterProvider = StateProvider.family<int, int>((ref, initialValue) {
+  ref.onDispose(() {
+    print('[familyCounterProvider($initialValue)] disposed');
+  });
+  return initialValue;
+});
